375d91334dc502ebf87487cd173321a92f0a102d
Koveanthan <154501772+Koveanthan@users.noreply.github.com>
2024-06-26 18:33:27 +0530

    fix issues with install.sh and remove .tmux.conf(move to a new dir)

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
-#!/usr/bin/sh
+#!/bin/bash
 
-CONFIG=~/.config
+# Check if GNU Stow is installed
+if ! command -v stow &> /dev/null
+then
+    echo "GNU Stow could not be found. Please install it and try again."
+    exit 1
+fi
 
+# Set the configuration directory
+CONFIG="$HOME/.config"
+
+# Export the CONFIG variable
 export CONFIG
 
-rm -rf $CONFIG
+# Create necessary directories if they do not exist
+mkdir -p "$CONFIG/nvim"
+mkdir -p "$CONFIG/yabai"
+mkdir -p "$CONFIG/skhd"
 
-mkdir -p $CONFIG/nvim/
-
-stow --verbose --restow --target=$CONFIG/nvim nvim
-stow --verbose --restow --target=$CONFIG/yabai yabai 
-stow --verbose --restow --target=$CONFIG/skhd skhd
-stow --verbose --restow --target=~/.tmux.conf .tmux.conf
+# Use stow to manage symlinks for configuration files
+stow --verbose --restow --target="$CONFIG/nvim" nvim
+stow --verbose --restow --target="$CONFIG/yabai" yabai
+stow --verbose --restow --target="$CONFIG/skhd" skhd
+stow --verbose --restow --target="$HOME" tmux
 
